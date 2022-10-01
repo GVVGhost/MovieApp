43e1504f4f1171be4d569f1ed52b91fc2796f021
GVVGhost <gvv.ghost@gmail.com>
2022-10-01 16:04:18 +0300

    Did some code cleanup

--- app/src/main/java/com/gvvghost/movieapp/retrofit/ApiService.java
+++ app/src/main/java/com/gvvghost/movieapp/retrofit/ApiService.java
@@ -13,11 +13,9 @@ public interface ApiService {
     @GET("discover/movie?language=en-US&sort_by=popularity.desc&vote_average.gte=4")
     Single<MovieResponse> loadMovies(@Query("api_key") String apiKey, @Query("page") int page);
 
-    //https://api.themoviedb.org/3/movie/616037/videos?api_key=4a4603c654a48486841a1c8013a81d6c&language=en-US
     @GET("movie/{movieId}/videos?language=en-US")
     Single<MovieDetailsResponse> loadMovieDetails(@Path("movieId") int id, @Query("api_key") String apiKey);
 
-    //https://api.themoviedb.org/3/movie/616037/reviews?api_key=4a4603c654a48486841a1c8013a81d6c&language=en-US&page=1
     @GET("movie/{movieId}/reviews?language=en-US&page=1")
     Single<MovieReviewResponse> loadMovieReviews(@Path("movieId") int id, @Query("api_key") String apiKey);
 }
